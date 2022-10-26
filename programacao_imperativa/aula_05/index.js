71de153ad9d96245389b65a9949c81d22e84b550
zamorini <pedrozamorini@gmail.com>
2022-10-25 22:13:48 -0300

    aula 5 de programacao imperativa - calc de imc

---
+++ programacao_imperativa/aula_05/index.js
@@ -0,0 +1,8 @@
+const nome = "Pedro Carmo"
+let idade = 22
+let peso = 69 
+let altura = 1.78
+
+let imc = peso / (altura*2)
+
+console.log(nome + ' tem ' + idade + ' anos e seu índice de massa corporal é de ' + imc.toFixed(2) );
